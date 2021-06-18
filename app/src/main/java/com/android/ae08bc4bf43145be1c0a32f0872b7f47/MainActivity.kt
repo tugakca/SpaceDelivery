03b9ed86d4a4ebee07b0c70bdf07ff1a49d58781
tugrulakcadag <tugrul@3pounds.io>
2021-06-18 21:27:03 +0300

    Initial commit

---
+++ app/src/main/java/com/android/ae08bc4bf43145be1c0a32f0872b7f47/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.android.ae08bc4bf43145be1c0a32f0872b7f47
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/test/java/com/android/ae08bc4bf43145be1c0a32f0872b7f47/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.android.ae08bc4bf43145be1c0a32f0872b7f47
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
